f9c6d506b8a08f4ebdd2fc4f0b0e8c167848dc5f
Abhinav Garg <agarg10@stevens.edu>
2021-04-21 00:10:23 -0400

    add settings to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import './css/App.css';
+import "./css/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Welcome to roadster</h1>
+        <h1>Welcome to roadster</h1>
       </header>
     </div>
   );
---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+const Landing = () => {
+  return (
+    <div>
+      Landing Page{" "}
+      <p>Welcome to the landing page. It will be fun. Enjoy your life</p>
+      <p>Welcome to Roadster</p>
+    </div>
+  );
+};
+
+export default Landing;
