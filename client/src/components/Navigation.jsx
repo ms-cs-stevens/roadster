1bfea46ad42749a9325bfab23f52b34213121cae
Abhinav Garg <agarg10@stevens.edu>
2021-04-22 16:09:12 -0400

    firebase

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-// import Signout from "./user/SignOut";
+import Signout from "./user/SignOut";
 
 const Navigation = () => {
   return (
     <header className="App-header">
       Roadster
-      {/* <Signout /> */}
+      <Signout />
     </header>
   );
 };
