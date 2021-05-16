1e7f2c2eed5e9ef0430e0a68995bddab11c47c5c
Abhinav Garg <agarg10@stevens.edu>
2021-05-16 17:45:50 -0400

    use react form to make front end validation easier

--- client/src/components/user/SignIn.jsx
+++ client/src/components/user/SignIn.jsx
@@ -1,48 +1,48 @@
-import React, { useContext } from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import { Redirect } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import { AuthContext } from '../../firebase/Auth';
-import { emailSignIn, passwordReset } from '../../firebase/firebaseFunctions';
-import SocialSignIn from './SocialSignIn';
-import banner from '../../images/sign-in-page.jpg';
+import React, { useContext } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import { Redirect } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { AuthContext } from "../../firebase/Auth";
+import { emailSignIn, passwordReset } from "../../firebase/firebaseFunctions";
+import SocialSignIn from "./SocialSignIn";
+import banner from "../../images/sign-in-page.jpg";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    height: '100vh',
+    height: "100vh",
   },
   image: {
     backgroundImage: `url(${banner})`,
-    backgroundRepeat: 'no-repeat',
+    backgroundRepeat: "no-repeat",
     backgroundColor:
-      theme.palette.type === 'light'
+      theme.palette.type === "light"
         ? theme.palette.grey[50]
         : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
+    backgroundSize: "cover",
+    backgroundPosition: "center",
   },
   paper: {
     margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -61,19 +61,19 @@ export default function SignInSide() {
     try {
       await emailSignIn(email.value, password.value);
     } catch (error) {
-      alert('Invalid Email or Password');
+      alert("Invalid Email or Password");
     }
   };
 
   const handlePasswordReset = (event) => {
     event.preventDefault();
-    const email = document.getElementById('email').value;
+    const email = document.getElementById("email").value;
     if (email) {
       passwordReset(email);
-      alert('Password reset email was sent');
+      alert("Password reset email was sent");
     } else {
       alert(
-        'Please enter an email address below before you click the forgot password link',
+        "Please enter an email address below before you click the forgot password link"
       );
     }
   };
@@ -116,11 +116,7 @@ export default function SignInSide() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <SocialSignIn />
+
             <Button
               type="submit"
               fullWidth
@@ -130,6 +126,7 @@ export default function SignInSide() {
             >
               Sign In
             </Button>
+
             <Grid container>
               <Grid item xs>
                 <Link href="#" variant="body2" onClick={handlePasswordReset}>
@@ -143,6 +140,11 @@ export default function SignInSide() {
               </Grid>
             </Grid>
           </form>
+          <br />
+          OR
+          <br />
+          <br />
+          <SocialSignIn />
         </div>
       </Grid>
     </Grid>
