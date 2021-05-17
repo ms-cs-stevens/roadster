fecf3f5870bf5cc5c2b084d6ee0b16ae0fc35394
Abhinav Garg <agarg10@stevens.edu>
2021-05-17 15:42:24 -0400

    complete journey filtering

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -79,7 +79,7 @@ export default function Navigation() {
               component={NavLink}
               to={"/journeys"}
             >
-              My Journeys
+              Journeys
             </Button>
           )}
           {currentUser && (
