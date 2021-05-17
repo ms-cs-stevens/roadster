2ebf551299c24f8446763e83ea6b2e508384b55f
neilvarunnaidu <neilvarunnaidu@icloud.com>
2021-05-17 00:05:17 -0400

    commentsv2

--- client/src/components/journeys/Comment.jsx
+++ client/src/components/journeys/Comment.jsx
@@ -1,33 +1,26 @@
-import React from 'react';
-import { formatRelative } from 'date-fns';
+import React from "react";
+import { formatRelative } from "date-fns";
 
-const formatDate = date => {
-  let formattedDate = '';
+const formatDate = (date) => {
+  let formattedDate = "";
   if (date) {
     formattedDate = formatRelative(date, new Date());
-    formattedDate = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+    formattedDate =
+      formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
   }
   return formattedDate;
 };
 
-const Comment = ({
-  timestamp = null,
-  content = '',
-  userName = '',
-}) => {
+const Comment = ({ timestamp = null, content = "", userName = "" }) => {
   if (!content) return null;
 
   return (
     <div>
       <div>
         <div>
-          {userName ? (
-            <p>{userName}</p>
-          ) : null}
+          {userName ? <p>{userName}</p> : null}
           {timestamp?.seconds ? (
-            <span>
-              {formatDate(new Date(timestamp.seconds * 1000))}
-            </span>
+            <span>{formatDate(new Date(timestamp.seconds * 1000))}</span>
           ) : null}
         </div>
         <p>{content}</p>
---
+++ client/src/components/journeys/CommentBox.jsx
@@ -0,0 +1,96 @@
+import firebase from "firebase/app";
+import 'firebase/auth'
+import 'firebase/firestore'
+import { useEffect } from "react";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyA-dW5JvHVfWdaMoUeLxP0DRcjQ89GCbg4",
+  authDomain: "roadster-sans.firebaseapp.com",
+  projectId: "roadster-sans",
+  storageBucket: "roadster-sans.appspot.com",
+  messagingSenderId: "653920987324",
+  appId: "1:653920987324:web:c656064aa53d57e1af08b9",
+  measurementId: "G-4Y94RV73E9"
+});
+
+const db = firebase.firestore();
+const query = db.collection('Comments').orderBy('timestamp').limit(100);
+
+const [loading, setLoading] = useState(true);
+const [user, setUser] = useState (() => auth.currentUser);
+const { userId, userName } = user;
+
+
+const CommentBox = ({ user = null }) => {
+    const [comments, setComments] = useState([]);
+    useEffect(()=>
+    {
+        const stop = firebase.auth().onAuthStateChanged(user=>{
+        if(user)    setUser(user);
+        else    setUser(false);
+
+        if(loading) setLoading(false);
+    });
+
+      return stop;
+    }, [loading]);
+
+    useEffect(() => {
+      const stop = query.onSnapshot(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+      }));
+      setComments(data);
+      });
+      return stop;
+    }, []);
+
+    const handleOnChange = e => {
+        setNewMessage(e.target.value);
+    };
+
+    const handleOnSubmit = e => {
+        e.preventDefault();
+      
+        const trimmed = newComment.trim();
+        if (trimmed) {
+          commentsRef.add({
+            content: trimmed,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            userId,
+            userName,
+            flagged: false,
+          });
+          setNewComment('');
+        }
+      };
+
+    return (
+        <div>
+        <ul>
+        {comments.map(comment => (
+            <li key={comment.id}>{comment.content}
+            <Comment { ...comment }/></li>
+        ))}
+        </ul>
+        <form
+        onSubmit={handleOnSubmit}>
+        <input
+        ref={inputRef}
+        type="text"
+        value={newComment}
+        onChange={handleOnChange}
+        />
+        <button
+        type="submit"
+        disabled={!newComment}
+        >
+        Post
+        </button>
+    </form>
+    </div>
+    );
+};
+
+export default CommentBox;
