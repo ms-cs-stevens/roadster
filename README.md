2632600712e89cd309c9361b6ff53a37d04e560a
neilvarunnaidu <neilvarunnaidu@icloud.com>
2021-05-19 23:39:06 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# roadster
+# Roadster
+
+Make sure you have MongoDB installed and running on your system.
+
+In a terminal(anywhere) type in the follwing to get the Redis server running on the default port: sudo service redis-server start
+
+Test whether the redis server is running by typing in: redis-cli ping
+It should return "PONG"
+
+Please check whether or not the key.json file is present at server\firebase
+
+Open 2 terminals in the project folder:
+Terminal 1-
+    cd server
+    npm i
+    npm run seed
+    npm start
+
+Terminal 2-
+    cd client
+    npm i
+    npm start
+
+The server will start running at localhost:4000
+The React client will start running at localhost:3000 automatically
+
+Core Features in the application:
+    Firebase authentication
+    Journeys CRUD
+    Create PDFs of a Journey to share
+    Google Location API to visualize a trip
+    Set a journey as customizable or non-customizable by co-travellers if any
+    Adding 1 or more images for a trip
+    Private group chat among co travellers(only)
+    Public comments on a journey
+    Caching of User data
---
+++ client/src/components/journeys/Comments.jsx
@@ -0,0 +1,155 @@
+import React, { useEffect, useState, useRef, useContext } from "react";
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  makeStyles,
+} from "@material-ui/core";
+import { AuthContext } from "../../firebase/Auth";
+import apiService from "../../services/apiService";
+import { formatRelative } from "date-fns";
+import firebaseApp from "../../firebase/firebaseApp";
+import "firebase/firestore";
+
+const Comments = ({ journey, db = firebaseApp.firestore() }) => {
+    const [newComment, setNewComment] = useState("");
+    const { currentUser } = useContext(AuthContext);
+
+    const q = db.collection("comments");
+    let allComments = useFirestoreQuery(
+        q.orderBy("createdAt", "desc"),
+    );
+
+    const newCommentRef = useRef();
+
+    const { posterName, journeyId } = data;
+
+    const comments = allComments.filter(function jId(comment) {
+        return comment.journeyId === data.journeyId;
+    });
+
+    useEffect(() => {
+        if (newCommentRef.current) newCommentRef.current.focus();
+    }, [newCommentRef, comments]);
+
+    const handlePostComment = async (e) => {
+        e.preventDefault();
+
+        if(db)
+        {
+            const trimmed = newComment.trim();
+            if (trimmed) {
+                let newCommentData = {
+                    posterId: currentUser.id,
+                    content: trimmed,
+                    postername: posterName,
+                    journeyId: journeyId,
+                    createdAt: new Date(),
+                };
+
+                comments.add(newCommentData);
+
+
+                let updateData = journey;
+                updateData.comments.push(newCommentData);
+
+                try {
+                    const data = await apiService.editResource(
+                    `journeys/${journey._id}`,
+                    updateData
+                    );
+                    setComments(data.comments);
+                } catch (e) {
+                    console.log(e);
+                }
+                setNewComment("");
+            }
+        }
+    };
+
+        const formatDate = (date) => {
+            let formattedDate = "";
+            if (date) {
+            formattedDate = formatRelative(date, new Date());
+            formattedDate =
+                formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
+            }
+            return formattedDate;
+        };
+
+        const align = (comment) => {
+            return data.posterName === comment.username ? "right" : "left";
+        };
+
+        const handleOnTyping = (e) => {
+            setNewComment(e.target.value);
+        };
+
+        const renderComments = () => {
+            return (
+            comments &&
+            comments.map((comment, i) => {
+                console.log(comment);
+                const [firstName, lastName] = comment.username.split(" ");
+                return (
+                <ListItem key={i}>
+                    <Grid container>
+                    <Grid item xs={12}>
+                        <ListItemAvatar align={align(comment)}>
+                        <Avatar alt={firstName} className={classes.avatar}>
+                            {(firstName[0] + lastName[0]).toUpperCase()}
+                        </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                        align={align(comment)}
+                        primary={comment.content}
+                        ></ListItemText>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <ListItemText
+                        align={align(comment)}
+                        secondary={
+                            comment.createdAt?.seconds
+                            ? formatDate(new Date(comment.createdAt.seconds * 1000))
+                            : ""
+                        }
+                        ></ListItemText>
+                    </Grid>
+                    </Grid>
+                </ListItem>
+                );
+            })
+            );
+        };
+
+    return (
+        <Card>
+        <CardHeader title="Comments Section" />
+        <CardContent max-height="400px">
+                <List>
+                {renderComments()}
+                </List>
+            {/* {comments?.map((comment) => (
+            <li key={comment.id}>{comment}</li>
+            ))} */}
+        </CardContent>
+        <Divider />
+        <form onSubmit={handlePostComment}>
+            <input
+            label={currentUser.posterName}
+            ref={newCommentRef}
+            type="text"
+            value={newComment}
+            onChange={handleOnTyping}
+            />
+            <button type="submit" disabled={!newComment}>
+            Post
+            </button>
+        </form>
+        </Card>
+    );
+};
+
+export default Comments;
